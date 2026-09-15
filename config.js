081ebdf045613507fb47a2c97f82d7b8ea2437b3
leiderlico <pueblofuertee@gmail.com>
2026-09-14 21:19:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ══════════════════════════════════════════════════════════════════
-// Pega aquí la URL /exec de tu Apps Script (Implementar > Nueva implementación).
+// Pega aquí la URL /exec de tu Apps Script (Ismplementar > Nueva implementación).
 // Es la MISMA URL que pones en Android en Constants.APPS_SCRIPT_URL.
 // ══════════════════════════════════════════════════════════════════
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxinkdQugX8-Tpi0ZF8qNw7nwAAl7SQ8K6VMpeuuL_sd3kDSTa4y-O8ixuLnh4rBUcl/exec";
